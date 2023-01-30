d9a57b41e343c92647fc9a2a5d5ed09e9e2cdebc
BOBsFood <123950443+BOBsFood@users.noreply.github.com>
2023-01-30 12:34:49 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function () {
+    serial.writeValue("x", input.acceleration(Dimension.X))
+    serial.writeValue("y", input.acceleration(Dimension.Y))
+    serial.writeValue("z", input.acceleration(Dimension.Z))
+})
